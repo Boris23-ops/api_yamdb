994f139cfd28ec8c8bafbf8c1fb64c4a2bbe7756
Борис Короткиян <borisrow23@yandex.ru>
2023-09-13 13:52:06 +0300

    #19

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets
 from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
 from users.models import User
-from users.serializers import UserSerializer
-from users.permission import IsAdmin
+from users.serializers import UserSerializer, UserMeSerializer
+from users.permission import IsAdmin, IsAuthenticated
 
 class UserViewSet(viewsets.ModelViewSet):
     """Вьюсет для работы с пользователями."""
@@ -15,3 +18,13 @@ class UserViewSet(viewsets.ModelViewSet):
     search_fields = ('username',)
     ordering = ('username',)
     http_method_names = ('get', 'post', 'patch', 'delete')
+
+
+class UserMeView(APIView):
+    """Вью-функция для работы с текущим пользователем."""
+
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        serializer = UserMeSerializer(request.user)
+        return Response(serializer.data, status=HTTP_200_OK)
