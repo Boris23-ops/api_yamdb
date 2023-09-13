d3fc6c7370e7fa259dd4a7a5ac3e2016c9132497
Александр Вторушин <mrazzzlop@gmail.com>
2023-09-13 12:15:05 +0700

    Модели

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -93,13 +93,13 @@ class Review(models.Model):
     titles = models.ForeignKey(
         Title, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.text[:MAX_TITLE_LENGTH]
-
     class Meta:
         unique_together = ('author', 'titles')
         default_related_name = 'reviews'
-        ordering = ['pub_date']
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.text[:MAX_TITLE_LENGTH]
 
 
 class Comment(models.Model):
@@ -108,11 +108,12 @@ class Comment(models.Model):
     review = models.ForeignKey(
         Review, on_delete=models.CASCADE)
     text = models.TextField()
-    created = models.DateTimeField(
+    pub_date = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+    class Meta:
+        ordering = ('id', )
+        default_related_name = 'comments'
 
     def __str__(self):
         return self.text[:MAX_TITLE_LENGTH]
-
-    class Meta:
-        default_related_name = 'comments'
